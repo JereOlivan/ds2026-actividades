b938d6d87a39e190ce7206d280823ad10d1b9691
Jere Olivan <jerebronzodia@gmail.com>
2026-04-20 20:58:19 -0300

    subo ej1

---
+++ ejercicios/c04-typecript/ej1-medio-arbol/app.ts
@@ -0,0 +1,31 @@
+
+const input = document.getElementById('inputAltura') as HTMLInputElement;
+const boton = document.getElementById('btnGenerar') as HTMLButtonElement;
+const display = document.getElementById('resultadoArbol') as HTMLPreElement;
+
+const generarAsteriscos = (n: number): string => {
+    let asteriscos: string = "";
+    for (let i = 0; i < n; i++) {
+        asteriscos += "*";
+    }
+    return asteriscos;
+}
+
+boton.addEventListener('click', () => {
+    const altura: number = parseInt(input.value); 
+
+    if (!input.value || altura < 1) {
+        display.style.color = "red";
+        display.innerText = "Error: Ingresá un número mayor a 0.";
+        return; 
+    }
+
+    display.style.color = "black"; 
+    let arbolCompleto: string = "";
+    
+    for (let i = 1; i <= altura; i++) {
+        arbolCompleto += generarAsteriscos(i) + "\n";
+    }
+
+    display.innerText = arbolCompleto;
+});
